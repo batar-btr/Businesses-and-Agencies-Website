f7f9b921f5f6da2b981902bf30481051bb466c8b
batar-btr <btrn86@gmail.com>
2017-07-05 16:44:48 +0300

    minor changes

--- app/js/main.js
+++ app/js/main.js
@@ -12,23 +12,25 @@ function showNav() {
 //    }else{
 //        navBar.style.opacity = 1;
 //    }
-    var i = 0;
-    for (i = 0; i < listItems.length; i++) {
+    var i = 0,
+        arrLength = listItems.length;
+    for (i = 0; i < arrLength; i += 1) {
 //        console.log(listItems[i]);
         
-        if (listItems[i].style.opacity == 1) {
+        if (listItems[i].style.opacity === "1") {
             
             listItems[i].style.transitionDelay = +("." + (6 - i)) / 2 + "s";
             listItems[i].style.opacity = 0;
-            listItems[i].style.transform = "translateX(-100%)";
+            listItems[i].style.transform = "translateX(0)";
         } else {
             listItems[i].style.transitionDelay = +("." + i) / 2 + "s";
             listItems[i].style.opacity = 1;
-            listItems[i].style.transform = "translateX(0)";
+            listItems[i].style.transform = "translateX(100%)";
         }
     }
     
-    setTimeout(alert('hello'),2000);
 }
 
 burgerBtn.addEventListener('click', showNav);
+
+
