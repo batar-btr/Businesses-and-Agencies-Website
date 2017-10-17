b007ed2be4b630aa32693c5fc4e668b2216f8356
batar-btr <btrn86@gmail.com>
2017-10-17 18:36:29 +0300

    minor changes

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,9 @@ var gulp       = require('gulp'), // Подключаем Gulp
     rename       = require('gulp-rename'), // Подключаем библиотеку для переименования файлов
     del          = require('del'), // Подключаем библиотеку для удаления файлов и папок
     imagemin     = require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
-    pngquant     = require('imagemin-pngquant'), // Подключаем библиотеку для работы с png
     cache        = require('gulp-cache'), // Подключаем библиотеку кеширования
     autoprefixer = require('gulp-autoprefixer'),
-    mqpacker     = require('css-mqpacker'),
-    wait         = require('gulp-wait'),
-    postcss      = require('gulp-postcss');// Подключаем библиотеку для автоматического добавления префиксов
+    wait         = require('gulp-wait');
 
 gulp.task('sass', function(){ // Создаем таск Sass
     return gulp.src('app/sass/**/*.sass')
@@ -95,59 +92,6 @@ gulp.task('clear', function () {
 
 gulp.task('default', ['watch']);
 
-gulp.task('mq', function(){
-    
-    
-    gulp.src('dist/css/global.css')
-        .pipe(postcss([mqpacker]))
-        .pipe(gulp.dest('dist/test'));
-});
 
 
-var reporter    = require('postcss-reporter');
-var stylelint   = require('stylelint');
 
-gulp.task("css-lint", function() {
-
-  // Stylelint config rules
-  var stylelintConfig = {
-    "rules": {
-      "block-no-empty": true,
-      "color-no-invalid-hex": true,
-      "declaration-colon-space-after": "always",
-      "declaration-colon-space-before": "never",
-      "function-comma-space-after": "always",
-      "function-url-quotes": "double",
-      "media-feature-colon-space-after": "always",
-      "media-feature-colon-space-before": "never",
-      "media-feature-name-no-vendor-prefix": true,
-      "max-empty-lines": 5,
-      "number-leading-zero": "never",
-      "number-no-trailing-zeros": true,
-      "property-no-vendor-prefix": true,
-      "rule-no-duplicate-properties": true,
-      "declaration-block-no-single-line": true,
-      "rule-trailing-semicolon": "always",
-      "selector-list-comma-space-before": "never",
-      "selector-list-comma-newline-after": "always",
-      "selector-no-id": true,
-      "string-quotes": "double",
-      "value-no-vendor-prefix": true
-    }
-  }
-
-  var processors = [
-    stylelint(stylelintConfig),
-    // Pretty reporting config
-    reporter({
-      clearMessages: true,
-      throwError: true
-    })
-  ];
-
-  return gulp.src(
-      // Stylesheet source:
-      'dist/test/global.css'
-    )
-    .pipe(postcss(processors));
-});
